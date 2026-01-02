b67a502fcc1f60b03940ebe2a0534bb1b7322546
istiakAHMEDsaad <istik2020@gmail.com>
2026-01-03 00:33:56 +0600

    added custom style config for tailwind.config.js

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,9 @@
-import { Text, View } from "react-native";
+import { Text, View } from 'react-native';
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Lets build movies app</Text>
+    <View className='flex-1 items-center justify-center'>
+      <Text className='text-5xl text-primary'>Lets build!</Text>
     </View>
   );
 }
